793b1508dba3c934a7c3ed1e2ada4653532701a9
hugh <hughbromund44@gmail.com>
2020-01-24 00:36:54 -0500

    basic grid view is working

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Hugh Bromund on 1/24/20.
+//  Copyright © 2020 Hugh Bromund. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
